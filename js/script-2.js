dfdef6b40217bfc49d56a8af1de4a9d71faf1d65
Liubov Khomych <kroka.luba@gmail.com>
2024-02-12 00:08:26 +0200

    parseFloat() without Number

--- js/script-2.js
+++ js/script-2.js
@@ -11,7 +11,7 @@ function optimizer(data) {
 
     let arrayFromEntries = Object.entries(data).map(([fruit, price]) => {   //створюємо новий масив з пар значень ["ключ", "значення"] об'єкта і для нього запускаємо метод .map
       
-        return [fruit.toLowerCase(), Number.parseFloat(price).toFixed(2)];   //який поверне новий масив пар значень, які вже будуть відредаговані до умов задачі
+        return [fruit.toLowerCase(), parseFloat(price).toFixed(2)];   //який поверне новий масив пар значень, які вже будуть відредаговані до умов задачі
     
     });
 
